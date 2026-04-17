d9101a69ea59ca64d1ed217a2f78a145aa9db914
isMrZhang <isMrZhang@users.noreply.github.com>
2026-04-17 07:59:16 +0000

    chore: remove temporary test script

--- test-build.js
+++
@@ -1,8 +0,0 @@
-const webpack = require('webpack');
-const config = require('./build/webpack.prod.js');
-webpack(config, (err, stats) => {
-  if (err) { console.error(err.stack || err); return; }
-  const info = stats.toJson();
-  if (stats.hasErrors()) { console.error(info.errors); }
-  if (stats.hasWarnings()) { console.warn(info.warnings); }
-});
